2fbef3d22cc3f5e342cc1b60851f8c31c53662ba
DavidSanchez <luigisperstar7@gmail.com>
2022-08-16 21:01:35 -0400

    ahora funciona con http pero falta revisar bien el response

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,6 +5,8 @@ import{
   Validators,
   FormBuilder
 } from '@angular/forms';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
+
 import { AlertController } from '@ionic/angular';
 
 @Component({
@@ -16,13 +18,47 @@ export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup
 
-  constructor(public fb: FormBuilder, public alertController: AlertController) { 
+  constructor(public fb: FormBuilder, public alertController: AlertController, private http: HttpClient) {
     this.formularioLogin = this.fb.group({
       'nombre': new FormControl("",Validators.required),
       'clave': new FormControl("",Validators.required)
 
     })
-    
+
+  }
+  fetchInvitados(usuario){
+    const headers = { 'content-type': 'application/json'};
+    this.http.post('http://localhost:10000/login',JSON.stringify(usuario),{'headers':headers}).subscribe(function(data) {
+      console.log(data);
+      if(data == 'true'){
+        console.log('ingresado');
+
+      }else{
+        const alert = this.alertController.create({
+          header: 'Quien te crees tu o.0',
+          message: 'Estos Datos no son Correcto',
+          buttons: ['Aceptar'],
+        });
+         alert.present();
+
+      }
+      // let invitados = JSON.parse(data);
+      // if(usuario.nombre == y.nombre && usuario.clave == y.clave){
+      //   console.log('ingresado');
+      // }else{
+      //   const alert = await this.alertController.create({
+      //     header: 'Quien te crees tu o.0',
+      //     message: 'Estos Datos no son Correcto',
+      //     buttons: ['Aceptar'],
+      //   });
+      //   await alert.present();
+
+      // }
+
+
+      // console.log(data);
+    });
+
   }
 
   ngOnInit() {
@@ -30,19 +66,8 @@ export class LoginPage implements OnInit {
  async ingresar(){
     var y = this.formularioLogin.value;
     var usuario = JSON.parse(localStorage.getItem('usuario'));
+    this.fetchInvitados(y);
 
-    if(usuario.nombre == y.nombre && usuario.clave == y.clave){
-      console.log('ingresado');
-    }else{
-      const alert = await this.alertController.create({
-        header: 'Quien te crees tu o.0',
-        message: 'Estos Datos no son Correcto',
-        buttons: ['Aceptar'],
-      });
-      await alert.present();
-      
-    }
-    
   }
 
 }
